a9fa244811f295b0e893753a2d01236d659d2abf
Noor <noor.hassan0892@gmail.com>
2024-05-31 19:23:41 +0300

    done

--- src/pages/home.js
+++ src/pages/home.js
@@ -5,8 +5,6 @@ const Home = () => {
     <div className="bg-gray-900 text-white min-h-screen flex items-center justify-center">
       <div className="max-w-3xl text-center">
         <h1 className="text-6xl font-bold mb-8">Welcome to Kuwait #1 Bank</h1>
-
-        <img src />
       </div>
     </div>
   );
